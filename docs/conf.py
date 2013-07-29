59b3e9b6717b892971c27219661409512880fec1
Brian Kent <bpkent@gmail.com>
2013-07-29 17:40:29 -0400

    more tweaking to get readthedocs to work.

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy']
+MOCK_MODULES = ['numpy', 'scipy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
