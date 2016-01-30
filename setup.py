78991b92fc4909ad6f9346980562e07dbdf8bda3
Brian Kent <bpkent@gmail.com>
2016-01-30 17:24:56 -0500

    Update setup script.

--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='debacl',
-    version='1.0',
+    version='1.1',
     description='DEnsity-BAsed CLustering',
     url='https://github.com/CoAxLab/DeBaCl',
     author='Brian Kent',
     author_email='bpkent@gmail.com',
     license='BSD',
     classifiers=[
-        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Scientific/Engineering'
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Visualization'
         ],
     packages=find_packages(),
-    install_requires=["prettytable"]
+    install_requires=["prettytable", "networkx"]
 )
